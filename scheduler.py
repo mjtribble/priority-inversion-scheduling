983969d576888be1adefec34d1d245bba0ae74c1
Melody <mjtribble2@gmail.com>
2017-11-25 13:39:03 -0700

    working for given input

--- scheduler.py
+++ scheduler.py
@@ -3,81 +3,130 @@ Author: Melody Tribble
 
 Created On: Nov 21, 2017
 """
-import threading
-from time import sleep
-
-# configuration parameters
-time = 0
 
 
 class Buffer:
-    # @param number is the job identifier
-    # @param buffer is the job buffer
-    # @param priority is the job's priority
-    def __init__(self, values):
-        self.values_l = values
+    def __init__(self, number, value):
+        self.number = number
+        self.value = value
+        self.j_list = []
+        # self.stop = False  # for thread termination
 
     # called when printing the object
     def __str__(self):
-        values = ''
-        for value in self.values_l:
-            values += str(value)
-        return values
+        return "Buffer %s <%s>" % (self.number, self.value)
+
+    def add_job(self, j):
+        self.j_list.append(j)
 
 
 class Job:
-    def __init__(self, number, buffer, priority, arrival_time, run_time):
+    def __init__(self, number, priority, arrival_time, run_time):
         self.number = number
-        self.buffer = buffer
         self.priority = priority
         self.arrival_time = arrival_time
         self.run_time = run_time
-        self.stop = False  # for thread termination
+        self.remaining_time = self.run_time
 
     # called when printing the object
     def __str__(self):
-        return 'T_%s%sT_%s' % (self.number, self.buffer, self.number)
 
-    def run(self):
-        print(threading.currentThread().getName() + ': Starting')
-        while True:
-            # run the job here!!!
-            # terminate
-            if self.stop:
-                print(threading.currentThread().getName() + ': Ending')
-                return
+        if self.number is 1:
+            return 'T_%s111T_%s' % (self.number, self.number)
+
+        elif self.number is 2:
+            time_complete = self.run_time - self.remaining_time
+            return 'T_%s%sT_%s' % (self.number, 'N' * time_complete, self.number)
+
+        else:
+            return 'T_%s333T_%s' % (self.number, self.number)
+
 
 if __name__ == '__main__':
 
-    object_L = []  # keeps track of objects, so we can kill their threads
+    # List of jobs to run
+    job_l = [(1, 3), (3, 2), (6, 3), (8, 1), (10, 2), (12, 3), (26, 1)]
+    processes_l = []
 
-    buffer_1 = Buffer([0, 0, 0])
-    buffer_2 = Buffer([])
+    # Create Buffers
+    buffer_1 = Buffer(1, '0')
+    buffer_2 = Buffer(2, 'N')
 
-    job_1 = Job(1, buffer_1, 3, 1, 3)
-    object_L.append(job_1)
+    # Create Jobs and add them to the appropriate Buffer
+    for i in job_l:
+        t = i[1]
+        arrival_t = i[0]
 
-    job_2 = Job(2, buffer_2, 2, 3, 10)
-    object_L.append(job_2)
+        if t is 1:
+            job = Job(number=1, priority=3, arrival_time=arrival_t, run_time=3)
+            buffer_1.add_job(job)
 
-    job_3 = Job(3, buffer_1, 1, 6, 3)
-    object_L.append(job_3)
+        if t is 2:
+            job = Job(number=2, priority=2, arrival_time=arrival_t, run_time=10)
+            buffer_2.add_job(job)
 
-    # start all the objects
-    thread_L = []
-    for obj in object_L:
-        thread_L.append(threading.Thread(name=obj.__str__(), target=obj.run))
+        if t is 3:
+            job = Job(number=3, priority=1, arrival_time=arrival_t, run_time=3)
+            buffer_1.add_job(job)
 
-    for t in thread_L:
-        t.start()
+        print("Job created : ", i)
 
-    sleep(2)
+    b1 = buffer_1.j_list
+    b2 = buffer_2.j_list
 
-    # join all threads
-    for o in object_L:
-        o.stop = True
-    for t in thread_L:
-        t.join()
+    time = 0
+    if b1[0].arrival_time <= b2[0].arrival_time:
+        processes_l.append(b1[0])
+        time = b1[0].arrival_time
+        b1.remove(b1[0])
+    else:
+        processes_l.append(b2[0])
+        time = b2[0].arrival_time
+        b2.remove(b2[0])
 
-    print("All simulation threads joined")
+    while processes_l:
+        current_job = processes_l[0]
+        current_job.arrival_time = time
+        finish_time = current_job.arrival_time + current_job.remaining_time
+        next_job = None
 
+        for i in b1:
+            bool = ((i.arrival_time < finish_time) and (i.priority == 3 and current_job.priority != 3)) or (
+                i.arrival_time == finish_time and current_job.priority != 3)
+            if bool:
+                next_job = i
+                b1.remove(i)
+                break
+
+        for j in b2:
+            if ((j.arrival_time < finish_time) and current_job.priority == 1) \
+                    or (j.arrival_time == finish_time):
+                next_job = j
+                b2.remove(j)
+                break
+
+        if next_job:  # add next job to the font of the list.
+            if next_job.arrival_time == finish_time:
+                current_job.remaining_time = 0
+                print('time %s, %s' % (time, current_job))
+                processes_l.remove(current_job)
+                time = finish_time
+
+            elif next_job.arrival_time > current_job.arrival_time:
+                current_job.remaining_time = current_job.remaining_time - (next_job.arrival_time - current_job.arrival_time)
+                print('time %s, %s' % (time, current_job))
+
+            processes_l.insert(0, next_job)
+
+            if next_job.arrival_time > time: time = next_job.arrival_time
+
+        else:  # if current_job is finished remove from process_l
+            print('time %s, %s' % (time, current_job))
+            processes_l.remove(current_job)
+            time = finish_time
+
+        # the process list is empty but still low priority jobs in buffer 1
+        if not processes_l and b1:
+            next_job = b1[0]
+            processes_l.insert(0, b1[0])
+            b1.remove(next_job)
